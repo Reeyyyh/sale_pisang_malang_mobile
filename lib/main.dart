e685eccf517bcb0bb7ddd520ae604e275accd6b6
Reeyyyh <rhnlnv@gmail.com>
2024-12-30 05:08:03 +0700

    add aunt controller in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sale_pisang_malang/app/modules/auth/services/auth_service.dart';
 import 'package:sale_pisang_malang/app/modules/home/views/start_page_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  Get.put(AuthService());
   runApp(
     const GetMaterialApp(
       debugShowCheckedModeBanner: false,
