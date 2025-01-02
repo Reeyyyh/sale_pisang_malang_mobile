54cc3debab5f1ae8eeee2241da72a217560bda73
Reeyyyh <rhnlnv@gmail.com>
2025-01-02 09:51:31 +0700

    use auth register

--- lib/app/modules/auth/controllers/signup_page_controller.dart
+++ lib/app/modules/auth/controllers/signup_page_controller.dart
@@ -1,55 +1,35 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sale_pisang_malang/app/modules/auth/services/auth_service.dart';
 import 'package:sale_pisang_malang/app/modules/auth/views/login_page_view.dart';
 
 class SignUpController extends GetxController {
-  final AuthService _authService = AuthService();
+  final AuthService _authService = Get.find<AuthService>();
 
-  // Controllers untuk input
   final nameController = TextEditingController();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-  // Reactive variable untuk password visibility
   var isPasswordHidden = true.obs;
+  final signUpFormKey  = GlobalKey<FormState>();
 
-  // Form key untuk validasi form
-  final formKey = GlobalKey<FormState>();
-
-  // Fungsi untuk toggle visibilitas password
   void togglePasswordVisibility() {
     isPasswordHidden.value = !isPasswordHidden.value;
   }
 
-  // Fungsi untuk melakukan registrasi
   Future<void> register() async {
-    if (!formKey.currentState!.validate()) return; // Pastikan form valid
+    if (!signUpFormKey.currentState!.validate()) return;
 
     String name = nameController.text.trim();
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
 
     try {
-      // Gunakan AuthService untuk registrasi
-      await _authService.register(email, password, name);
-
-      // Ambil UID pengguna setelah berhasil registrasi
-      String uid = FirebaseAuth.instance.currentUser!.uid;
-
-      // Simpan data pengguna di Firestore
-      await FirebaseFirestore.instance.collection('users').doc(uid).set({
-        'name': name,
-        'email': email,
-        'role': email == 'adminpage@gmail.com' ? 'admin' : 'user',
-      });
-
-      // Redirect ke halaman StartPageView
+      await _authService.register(name, email, password);
       Get.offAll(() => LoginPageView());
       Get.snackbar("Registration Success", "Welcome, $name!");
     } catch (e) {
+      print('Registrasi gagal: ${e.toString()}');
       Get.snackbar("Registration Failed", e.toString());
     }
   }
